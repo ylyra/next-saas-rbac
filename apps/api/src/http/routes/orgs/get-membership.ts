2d7ed83c95c4fe2200b952e093792dec970ff337
Yan Lyra <yanlyra3@gmail.com>
2024-04-27 19:53:09 -0300

    feat: add route to get membership with org

---
+++ apps/api/src/http/routes/orgs/get-membership.ts
@@ -0,0 +1,58 @@
+import { roleSchema } from '@saas/auth'
+import type { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+import { createRoute } from '@/utils/create-route'
+
+import { ORGANIZATIONS_ROUTE_PREFIX, ORGANIZATIONS_TAGS } from '.'
+
+export async function orgsGetMembership(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .get(
+      createRoute(
+        ORGANIZATIONS_ROUTE_PREFIX,
+        ':organizationSlug',
+        'membership',
+      ),
+      {
+        schema: {
+          tags: ORGANIZATIONS_TAGS,
+          summary: 'Get the current user membership in an organization',
+          security: [
+            {
+              bearerAuth: [],
+            },
+          ],
+          params: z.object({
+            organizationSlug: z.string(),
+          }),
+          response: {
+            200: z.object({
+              membership: z.object({
+                id: z.string(),
+                role: roleSchema,
+                organizationId: z.string(),
+              }),
+            }),
+          },
+        },
+      },
+      async (request) => {
+        const { organizationSlug } = request.params
+
+        const { membership } = await request.getUserMembership(organizationSlug)
+
+        return {
+          membership: {
+            id: membership.id,
+            role: membership.role,
+            organizationId: membership.organizationId,
+          },
+        }
+      },
+    )
+}
--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -11,6 +11,7 @@ import { AppAbility } from './subjects'
 export * from './models/organization'
 export * from './models/project'
 export * from './models/user'
+export * from './roles'
 
 export const createAppAbility = createMongoAbility as CreateAbility<AppAbility>
 
