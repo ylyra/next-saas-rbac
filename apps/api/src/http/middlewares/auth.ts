88581fe4c74528f597d489e9e78e037fe9b30065
Yan Lyra <yanlyra3@gmail.com>
2024-04-27 19:47:17 -0300

    feat: add new middleware to return user membership with organization

--- apps/api/src/http/middlewares/auth.ts
+++ apps/api/src/http/middlewares/auth.ts
@@ -1,6 +1,8 @@
 import type { FastifyInstance } from 'fastify'
 import fastifyPlugin from 'fastify-plugin'
 
+import { prisma } from '@/lib/prisma'
+
 import { UnauthorizedError } from '../routes/_errors/unauthorized-error'
 
 export const auth = fastifyPlugin(async (app: FastifyInstance) => {
@@ -12,6 +14,33 @@ export const auth = fastifyPlugin(async (app: FastifyInstance) => {
         return payload.sub
       } catch {
         throw new UnauthorizedError('Invalid auth token')
+      }
+    }
+
+    request.getUserMembership = async (organizationSlug: string) => {
+      const userId = await request.getCurrentUserId()
+
+      const member = await prisma.member.findFirst({
+        where: {
+          userId,
+          organization: {
+            slug: organizationSlug,
+          },
+        },
+        include: {
+          organization: true,
+        },
+      })
+
+      if (!member) {
+        throw new UnauthorizedError('You are not a member of this organization')
+      }
+
+      const { organization, ...membership } = member
+
+      return {
+        organization,
+        membership,
       }
     }
   })
