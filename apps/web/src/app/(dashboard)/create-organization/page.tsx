c487ce82e52d71b6fa289eadb9fffefdb2d5c485
Yan Lyra <yanlyra3@gmail.com>
2024-06-09 16:40:50 -0300

    chore: return header to pages because it does not rerender when page changes

--- apps/web/src/app/(dashboard)/create-organization/page.tsx
+++ apps/web/src/app/(dashboard)/create-organization/page.tsx
@@ -1,11 +1,17 @@
+import { Header } from '@/components/header'
+
 import OrganizationForm from './organization-form'
 
 export default async function Page() {
   return (
-    <div className="space-y-4">
-      <h1 className="text-2xl font-bold">Create Organization</h1>
+    <div className="space-y-4 py-4">
+      <Header />
 
-      <OrganizationForm />
+      <main className="container space-y-4">
+        <h1 className="text-2xl font-bold">Create Organization</h1>
+
+        <OrganizationForm />
+      </main>
     </div>
   )
 }
--- apps/web/src/app/(dashboard)/layout.tsx
+++ apps/web/src/app/(dashboard)/layout.tsx
@@ -2,18 +2,11 @@ import { redirect } from 'next/navigation'
 import type { PropsWithChildren } from 'react'
 
 import { isAuthenticated } from '@/auth/auth'
-import { Header } from '@/components/header'
 
 export default function Template({ children }: PropsWithChildren) {
   if (!isAuthenticated()) {
     redirect('/auth/sign-in')
   }
 
-  return (
-    <div className="space-y-4 py-4">
-      <Header />
-
-      <main className="container">{children}</main>
-    </div>
-  )
+  return <>{children}</>
 }
