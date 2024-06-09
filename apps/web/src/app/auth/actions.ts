4a81d48787126acee0eb200dd4ba43aa6682270e
Yan Lyra <yanlyra3@gmail.com>
2024-06-09 15:54:53 -0300

    chore: wip

--- apps/web/src/app/auth/actions.ts
+++ apps/web/src/app/auth/actions.ts
@@ -1,15 +1,13 @@
 'use server'
 
+import { env } from '@saas/env'
 import { redirect } from 'next/navigation'
 
 export async function signInWithGithub() {
   const githubSignInUrl = new URL('login/oauth/authorize', 'https://github.com')
 
-  githubSignInUrl.searchParams.set('client_id', 'bcae7ddbdc5a05f28dfc')
-  githubSignInUrl.searchParams.set(
-    'redirect_uri',
-    'http://localhost:3000/api/auth/callback',
-  )
+  githubSignInUrl.searchParams.set('client_id', env.GITHUB_CLIENT_ID)
+  githubSignInUrl.searchParams.set('redirect_uri', env.GITHUB_REDIRECT_URL)
   githubSignInUrl.searchParams.set('scope', 'user')
 
   redirect(githubSignInUrl.toString())
